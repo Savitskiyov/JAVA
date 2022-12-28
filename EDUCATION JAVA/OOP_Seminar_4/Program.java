d9d93e025c260e5b1774c3b6875917fca210ab89
«savitskiyov» <savitskiyov@gmail.com>
2022-12-28 20:54:18 +0300

    Домашнее задание 4

---
+++ EDUCATION JAVA/OOP_Seminar_4/Program.java
@@ -0,0 +1,15 @@
+package OOP_Seminar_4;
+
+public class Program {
+    public static void main(String[] args) {
+        FamilyTree<Human> familyTree = new FamilyTree<Human>();
+        UserMenu menu = new UserMenu();
+
+        menu.fillHumans(familyTree);
+
+        while (menu.getMenuStatus()) {
+            familyTree = menu.launchMenu(familyTree);
+        }
+    }
+
+}
